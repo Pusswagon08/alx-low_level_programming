bfd7f6ea79a8d1e65d949e88bf5592bc8aca3e7f
Pussywagon08 <rilwanabioye08@gmail.com>
2023-03-17 20:37:50 +0100

    6666

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,9 @@
 
 /**
  * main - Entry point
- * Description: Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * 
+ *Description: 'the program's description'
+
  * Return: Always 0 on success
  */
 
