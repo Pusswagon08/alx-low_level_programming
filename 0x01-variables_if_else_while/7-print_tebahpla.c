297c94704c3bc31d219ea0245298f019a3c3371e
Pussywagon08 <rilwanabioye08@gmail.com>
2023-03-17 20:16:33 +0100

    dumb-me

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - main code function
+ * main - main function
  *
- * Return: Always return 0
+ * Return: always 0
  */
 
 int main(void)
 {
+	char i;
 
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
