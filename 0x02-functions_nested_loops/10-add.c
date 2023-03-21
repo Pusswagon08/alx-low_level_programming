a9166e53f7a06a5c6b8a671139c9728945a3b684
Pussywagon08 <rilwanabioye08@gmail.com>
2023-03-21 14:41:17 -0700

    task-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integer
+ *
+ * @a,@b: parameter a and b are considered
+ *
+ * Return: return the value of c
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
