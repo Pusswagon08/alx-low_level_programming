2460e063478a315e4fe1ccf445e88d5ecfbe94b9
Pussywagon08 <rilwanabioye08@gmail.com>
2023-03-21 08:49:45 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main code block
+ *
+ * Description: a program that prints _putchar, followed by a new line.
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
