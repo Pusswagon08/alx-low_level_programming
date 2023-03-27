b95b538dddbdaced9fd1bbd783a8a49b6f273a74
Pussywagon08 <Pussywagon08>
2023-03-27 13:17:33 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * @a: parameter a
+ *
+ * @b: parameter b
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
