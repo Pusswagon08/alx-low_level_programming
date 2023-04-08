b08556a6d3fb370d8a9d4b0e4912d421e208a331
Pussywagon08 <Pussywagon08>
2023-04-08 13:32:52 -0700

    task-1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main code block
+ *
+ * Description: program that prints the number of arguement passed to it
+ *
+ * @argc: arguement counter
+ *
+ * @argv: stores arguement's name
+ *
+ * Return: 0 on (success)
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	int i = argc -1;
+
+	printf("%d\n", i);
+	return (0);
+}
